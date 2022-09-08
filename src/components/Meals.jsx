6ec3c143025914492c16d7469e1aaa6b20913d52
Federico Jimenez <fedeejimenez@gmail.com>
2022-09-08 19:44:21 +0000

    Display fetched meals

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,11 +1,20 @@
 import { useGlobalContext } from '../context'
 
 const Meals = () => {
-	const context = useGlobalContext()
-	console.log(context)
-	return (
-		<h1>Meals</h1>
-	)
+	const { meals } = useGlobalContext()
+
+	return <section className='section-center'>
+		{meals.map((singleMeal) => {
+			const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+			return <article key={idMeal} className='single-meal'>
+				<img src={image} style={{ width: '200px' }} className="img" />
+				<footer>
+					<h5>{title}</h5>
+					<button className='like-btn'>Click Me</button>
+				</footer>
+			</article>
+		})}
+	</section>
 }
 
 export default Meals
