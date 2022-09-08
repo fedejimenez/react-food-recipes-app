9206f4fb83bd2bd39991264c0137d933e92aef72
Federico Jimenez <fedeejimenez@gmail.com>
2022-09-08 06:18:38 +0000

    Add styles and set up project main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import Favorites from './components/Favorites';
+import Meals from './components/Meals';
+import Modal from './components/Modal';
+import Search from './components/Search';
 
 export default function App() {
 	return (
 		<main>
-			Food Recipes
+			{/* <Search /> */}
+			{/* <Favorites /> */}
+			<Meals />
+			{/* <Modal /> */}
 		</main>
 	)
 }
