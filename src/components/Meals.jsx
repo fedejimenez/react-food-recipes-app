a02ecd5e08fdca2719bb754aa2522e9f5d48bf8b
Federico Jimenez <fedeejimenez@gmail.com>
2022-09-08 20:12:38 +0000

    Add message when there is no fetched data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -8,20 +8,26 @@ const Meals = () => {
 		return <section className="section">
 			<h4>Loading...</h4>
 		</section>
-	} else {
-		return <section className='section-center'>
-			{meals.map((singleMeal) => {
-				const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
-				return <article key={idMeal} className='single-meal'>
-					<img src={image} className="img" />
-					<footer>
-						<h5>{title}</h5>
-						<button className='like-btn'><BsHandThumbsUp /></button>
-					</footer>
-				</article>
-			})}
+	}
+
+	if (meals.length < 1) {
+		return <section className="section">
+			<h4>No meals matced your search term. Please try something different.</h4>
 		</section>
 	}
+
+	return <section className='section-center'>
+		{meals.map((singleMeal) => {
+			const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+			return <article key={idMeal} className='single-meal'>
+				<img src={image} className="img" />
+				<footer>
+					<h5>{title}</h5>
+					<button className='like-btn'><BsHandThumbsUp /></button>
+				</footer>
+			</article>
+		})}
+	</section>
 }
 
 export default Meals
