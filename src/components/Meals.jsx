ba72972434e687a93c4cb92f18faf02cd11e052b
Federico Jimenez <fedeejimenez@gmail.com>
2022-09-08 20:01:39 +0000

    Improve styling and add loading message

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,20 +1,27 @@
 import { useGlobalContext } from '../context'
+import { BsHandThumbsUp } from 'react-icons/bs'
 
 const Meals = () => {
-	const { meals } = useGlobalContext()
+	const { loading, meals } = useGlobalContext()
 
-	return <section className='section-center'>
-		{meals.map((singleMeal) => {
-			const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
-			return <article key={idMeal} className='single-meal'>
-				<img src={image} style={{ width: '200px' }} className="img" />
-				<footer>
-					<h5>{title}</h5>
-					<button className='like-btn'>Click Me</button>
-				</footer>
-			</article>
-		})}
-	</section>
+	if (loading) {
+		return <section className="section">
+			<h4>Loading...</h4>
+		</section>
+	} else {
+		return <section className='section-center'>
+			{meals.map((singleMeal) => {
+				const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+				return <article key={idMeal} className='single-meal'>
+					<img src={image} className="img" />
+					<footer>
+						<h5>{title}</h5>
+						<button className='like-btn'><BsHandThumbsUp /></button>
+					</footer>
+				</article>
+			})}
+		</section>
+	}
 }
 
 export default Meals
